3b72a3b5aa0bdb54f44afa54f106b6f1ba4b13d8
conniely <conniely04@gmail.com>
2024-01-20 12:35:10 -0800

    changes

--- app/app/page.js
+++ app/app/page.js
@@ -1,15 +1,18 @@
 import RootLayout from "./layout";
 import styles from "./home.module.css";
+import PersonIcon from "@mui/icons-material/Person";
+import Tree from "./components/Tree";
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.format}>
       <div className={styles.container}>
         {/* Other content of your page */}
         <RootLayout>
           <div className={styles.titleformat}>
             <h1 className={styles.welcome}> Welcome home, </h1>
             <h1 className={styles.username}> User</h1>
+            <PersonIcon className={styles.icon} />
           </div>
         </RootLayout>
       </div>
