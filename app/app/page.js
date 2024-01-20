2d31279a00f8c0d778ee089290129efc6cf985a5
Bill Zhang <billzhangsc@gmail.com>
2024-01-20 13:15:07 -0800

    Authentication and mongodb works

--- app/app/page.js
+++ app/app/page.js
@@ -1,13 +1,16 @@
-import RootLayout from './layout'
 import styles from './home.module.css'
 import PersonIcon from '@mui/icons-material/Person'
-import Tree from './components/Tree'
+import {getSession} from '@auth0/nextjs-auth0'
 
-export default function Home() {
+// import Tree from './components/Tree'
+
+export default async function Home() {
+  const {user} = await getSession()
+  console.log(user)
   return (
     <div className={styles.titleformat}>
       <h1 className={styles.welcome}> Welcome home, </h1>
-      <h1 className={styles.username}> User</h1>
+      <h1 className={styles.username}> {user?.name}</h1>
       <PersonIcon className={styles.icon} />
     </div>
   )
