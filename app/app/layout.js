855a538ec770fc7d4232f28d20b8a4e60184c544
Bill Zhang <billzhangsc@gmail.com>
2024-01-20 12:40:05 -0800

    Fix changes

--- app/app/layout.js
+++ app/app/layout.js
@@ -1,20 +1,21 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import BottomNav from "./components/BottomNavbar.js";
+import {Inter} from 'next/font/google'
+import './globals.css'
+import BottomNav from './components/BottomNavbar.js'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ['latin']})
 
 export const metadata = {
-  title: "BeSustainable",
-  description: "BeReal but for food",
-};
+  title: 'BeSustainable',
+  description: 'BeReal but for food',
+}
 
-export default function RootLayout({ children }) {
+export default function RootLayout({children}) {
   return (
-    <>
-      <BottomNav />
-
-      <main>{children}</main>
-    </>
-  );
+    <html lang="en">
+      <body>
+        <BottomNav />
+        {children}
+      </body>
+    </html>
+  )
 }
--- app/app/profile/page.js
+++ app/app/profile/page.js
@@ -1,12 +1,6 @@
-import styles from "./profile.module.css";
-import RootLayout from "../layout";
+import styles from './profile.module.css'
+import RootLayout from '../layout'
 
 export default function Profile() {
-  return (
-    <main>
-      <RootLayout>
-        <h1 className={styles.text}> Welcome to the Profile Page </h1>
-      </RootLayout>
-    </main>
-  );
+  return <h1 className={styles.text}> Welcome to the Profile Page </h1>
 }
