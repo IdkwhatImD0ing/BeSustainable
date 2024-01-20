b2bfac52f05a3fb66f313f123fb9c3494612280f
Restyled.io <commits@restyled.io>
2024-01-20 22:51:50 +0000

    Restyled by clang-format

--- app/app/schemas/post-db.js
+++ app/app/schemas/post-db.js
@@ -2,54 +2,54 @@ const User = require('../schemas/userModel.js')
 const Post = require('../schemas/postModel.js');
 
 async function createPost(userId, imageLink, caption) {
-    try {
-        const newPost = new Post({imageLink, caption});
-        await newPost.save();
+  try {
+    const newPost = new Post({imageLink, caption});
+    await newPost.save();
 
-        const user = await User.findById(userId);
-        if(!user) {
-            throw new Error('User not found');
-        }
-
-        newPost.nextPost = user.headPost;
-        await newPost.save();
-
-        user.headPost = newPost._id;
-        await user.save();
-
-        return newPost;
-    } catch (error) {
-        console.error('Error creating post for user: ', error);
-        throw error;
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new Error('User not found');
     }
+
+    newPost.nextPost = user.headPost;
+    await newPost.save();
+
+    user.headPost = newPost._id;
+    await user.save();
+
+    return newPost;
+  } catch (error) {
+    console.error('Error creating post for user: ', error);
+    throw error;
+  }
 }
 
 async function getPosts(userId) {
-    try {
-        const user = await User.findById(userId);
-        if(!user || !user.headPost) {
-            throw new Error("User not found");
-        }
-
-        let posts = [];
-        let currentPostId = user.headPost;
-        while(currentPostId) {
-            const post = await Post.findById(currentPostId);
-            if(!post) break;
-
-            posts.push(post);
-            currentPostId = post.nextPost;
-        }
-
-        return posts;
-    } catch(error) {
-        consoler.error('Error retrieving posts: ', error);
-        throw error;
+  try {
+    const user = await User.findById(userId);
+    if (!user || !user.headPost) {
+      throw new Error("User not found");
     }
+
+    let posts = [];
+    let currentPostId = user.headPost;
+    while (currentPostId) {
+      const post = await Post.findById(currentPostId);
+      if (!post)
+        break;
+
+      posts.push(post);
+      currentPostId = post.nextPost;
+    }
+
+    return posts;
+  } catch (error) {
+    consoler.error('Error retrieving posts: ', error);
+    throw error;
+  }
 }
 
 module.exports = {
-    createPost,
-    getPosts
+  createPost,
+  getPosts
 };
-
--- app/app/schemas/user-db.js
+++ app/app/schemas/user-db.js
@@ -1,47 +1,46 @@
 const User = require("../schemas/userModel.js");
 
 async function createUser(username, email, password) {
-    try {
-        const newUser = new User({
-            username: username,
-            email: email,
-            password: password,
-        });
+  try {
+    const newUser = new User({
+      username : username,
+      email : email,
+      password : password,
+    });
 
-        await newUser.save();
+    await newUser.save();
 
-        console.log("User created: ", newUser);
+    console.log("User created: ", newUser);
 
-        return newUser;
-    } catch (error) {
-        console.error("Error creating user: ", error);
-        throw error;
-    }
+    return newUser;
+  } catch (error) {
+    console.error("Error creating user: ", error);
+    throw error;
+  }
 }
 
 async function getUserById(userId) {
-    try {
-        const user = await User.findById({id: userId});
-        return user;
-    } catch(error) {
-        console.error("Error finding user: ", error);
-        throw error;
-    }
+  try {
+    const user = await User.findById({id : userId});
+    return user;
+  } catch (error) {
+    console.error("Error finding user: ", error);
+    throw error;
+  }
 }
 
 async function getUserByEmail(email) {
-    try {
-        const user = await User.findOne({email: email});
-        return user;
-    } catch (error) {
-        consoler.error('Error finding user by email: ', error);
-        throw error;
-    }
+  try {
+    const user = await User.findOne({email : email});
+    return user;
+  } catch (error) {
+    consoler.error('Error finding user by email: ', error);
+    throw error;
+  }
 }
 
 module.exports = {
-    createUser,
-    getUserById,
-    getUserByEmail
+  createUser,
+  getUserById,
+  getUserByEmail
 }
-
