e25b4b8a8af532a86fd0bc95a97f8c3fe49e23e0
Bill Zhang <billzhangsc@gmail.com>
2024-01-20 20:09:34 -0800

    Finished backend

--- app/app/api/sustainability/utils.js
+++ app/app/api/sustainability/utils.js
@@ -1,15 +1,18 @@
 import scores from './scores.json'
 
-function getScore(ingredient) {
+function getScore(ingredients) {
   // Simply find average of all scores, ignore ingredient not in scores.json
+  // ingredients is a list of strings
   let totalScore = 0
   let count = 0
-  for (const key in ingredient) {
-    if (scores[key]) {
-      totalScore += scores[key]
+  for (let i = 0; i < ingredients.length; i++) {
+    const ingredient = ingredients[i]
+    if (ingredient in scores) {
+      totalScore += scores[ingredient]
       count++
     }
   }
+
   return totalScore / count
 }
 
